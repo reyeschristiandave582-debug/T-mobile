3b5355c9de37cde4dbd626fc2ed664861fa803c4
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-09 00:50:57 +0800

    Update main-logo.tsx

--- src/components/sections/main-logo.tsx
+++ src/components/sections/main-logo.tsx
@@ -6,11 +6,11 @@ const MainLogo = () => {
   return (
     <div 
       className="w-full flex justify-center pt-2 pb-0 cursor-pointer"
-      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=4280&aff_id=183864&source=Coach" } }, "*")}
+      onClick={() => window.parent.postMessage({ type: "OPEN_EXTERNAL_URL", data: { url: "https://giftclick.org/aff_c?offer_id=4414&aff_id=183864&source=T-Mobile" } }, "*")}
     >
       <img 
         src="https://i.imgur.com/E44tt5A.png" 
-        alt="Coach Logo" 
+        alt="T-Mobile Logo" 
         className="h-10 sm:h-15 w-20 object-contain transition-all duration-700 hover:brightness-120"
       />
     </div>
